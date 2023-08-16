ee1de0332f73d97081359fd892a45a7501fb926f
Alex Buzea <alexbuzea@ymail.com>
2023-08-16 10:20:04 +0100

    more config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react_simple_to_do/", // Add this line
+  base: "/react_simple_to_do/",
   plugins: [react()],
 });
